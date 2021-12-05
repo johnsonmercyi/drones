6e79bd632da2131a4a7f7adf9a6d7fe751b7f9d0
Johnson Mercy I <you@example.com>
2021-12-06 00:59:57 +0100

    My first commit

---
+++ src/test/java/com/soft/test/DroneDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soft.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
