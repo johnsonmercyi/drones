af9fd914743cd246a6c71ed2c83473eb0ef6e888
Johnson Mercy I <you@example.com>
2021-12-06 16:26:52 +0100

    Db Migration V 1.4 created to check drone battery capacity and alter drones column: battery_capacity

---
+++ src/main/resources/db/migration/V1_4__DroneDDL.sql
@@ -0,0 +1,12 @@
+ALTER TABLE drones ALTER COLUMN battery_capacity
+SET DEFAULT (100);
+
+CREATE TABLE battery_check_logs (
+    id                      INT NOT NULL DEFAULT RANDOM_UUID(),
+    drone_id                UUID NOT NULL,
+    battery_capacity        INTEGER NOT NULL,
+    checked_time            TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (drone_id) REFERENCES drones (id)
+);
